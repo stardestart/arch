9dab25f715a61e4a475ecae124604794f5c611ac
stardestart <99838873+stardestart@users.noreply.github.com>
2022-11-16 13:07:11 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -163,16 +163,16 @@ echo '[multilib]
 Include = /etc/pacman.d/mirrorlist' >> /mnt/etc/pacman.conf
 echo 'kernel.sysrq=1' > /mnt/etc/sysctl.d/99-sysctl.conf
 arch-chroot /mnt pacman -Sy reflector --noconfirm
-if [ -z "$cpu" ];
-then
-arch-chroot /mnt pacman -Sy amd-ucode --noconfirm;
-else
-arch-chroot /mnt pacman -Sy intel-ucode iucode-tool --noconfirm;
-fi
-if [ "$gpu" == "amd" ]; then arch-chroot /mnt pacman -Sy amdvlk;
-elif
-[ "$gpu" == "nvidia" ]; then arch-chroot /mnt pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia lib32-opencl-nvidia opencv-cuda nvtop cuda;
-fi
+#if [ -z "$cpu" ];
+#then
+#arch-chroot /mnt pacman -Sy amd-ucode --noconfirm;
+#else
+#arch-chroot /mnt pacman -Sy intel-ucode iucode-tool --noconfirm;
+#fi
+#if [ "$gpu" == "amd" ]; then arch-chroot /mnt pacman -Sy amdvlk;
+#elif
+#[ "$gpu" == "nvidia" ]; then arch-chroot /mnt pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia lib32-opencl-nvidia opencv-cuda nvtop cuda;
+#fi
 arch-chroot /mnt sed -i 's/# --country France,Germany/--country Finland,Germany,Russia/' /etc/xdg/reflector/reflector.conf
 arch-chroot /mnt pacman -Sy xorg i3-gaps xorg-xinit xorg-apps xterm dmenu xdm-archlinux i3status git firefox numlockx ark mc htop conky polkit network-manager-applet dolphin ntfs-3g dosfstools qt5ct lxappearance-gtk3 papirus-icon-theme picom redshift tint2 grc flameshot xscreensaver notification-daemon adwaita-qt5 gnome-themes-extra alsa-utils alsa-plugins lib32-alsa-plugins alsa-firmware alsa-card-profiles pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol freetype2 noto-fonts-extra noto-fonts-cjk cheese kwrite mesa lib32-mesa go wireless_tools packagekit-qt5 winetricks wine --noconfirm
 arch-chroot /mnt/ sudo -u $username sh -c "cd /home/$username/; git clone https://aur.archlinux.org/yay.git; cd /home/$username/yay; BUILDDIR=/tmp/makepkg makepkg -i --noconfirm"
