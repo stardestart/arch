f3c7dacdd1b6a7221c1217e5fcf9e525df0e87f9
stardestart <99838873+stardestart@users.noreply.github.com>
2022-11-16 18:01:24 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -59,19 +59,19 @@ n
 +512m
 n
 2
-
+\n
 +1m
 t
 2
 4
 n
 3
-
+\n
 +1g
 n
 4
-
-
+\n
+\n
 w
 EOF
 mkfs.ext2 /dev/${disk}1 -L boot<<EOF
@@ -95,12 +95,12 @@ t
 1
 n
 2
-
+\n
 +1g
 n
 3
-
-
+\n
+\n
 w
 EOF
 mkfs.fat -F32 /dev/${disk}1 -n boot
@@ -156,10 +156,10 @@ initrd /$microcode.img
 initrd  /initramfs-linux-zen.img
 options root=/dev/${disk}3 rw" > /mnt/boot/loader/entries/arch.conf;
 fi
-if [ -z "$cpu" ];
+if [ "$cpu" == "amd-ucode" ];
 then
 arch-chroot /mnt pacman -Sy amd-ucode --noconfirm;
-else
+elif [ "$cpu" == "intel-ucode" ];
 arch-chroot /mnt pacman -Sy intel-ucode iucode-tool --noconfirm;
 fi
 arch-chroot /mnt sed -i 's/#Color/Color/' /etc/pacman.conf
@@ -168,8 +168,7 @@ Include = /etc/pacman.d/mirrorlist' >> /mnt/etc/pacman.conf
 echo 'kernel.sysrq=1' > /mnt/etc/sysctl.d/99-sysctl.conf
 arch-chroot /mnt pacman -Sy reflector --noconfirm
 if [ "$gpu" == "amd" ]; then arch-chroot /mnt pacman -Sy amdvlk;
-elif
-[ "$gpu" == "nvidia" ]; then arch-chroot /mnt pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia lib32-opencl-nvidia opencv-cuda nvtop cuda;
+elif [ "$gpu" == "nvidia" ]; then arch-chroot /mnt pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia lib32-opencl-nvidia opencv-cuda nvtop cuda;
 fi
 arch-chroot /mnt sed -i 's/# --country France,Germany/--country Finland,Germany,Russia/' /etc/xdg/reflector/reflector.conf
 arch-chroot /mnt pacman -Sy xorg i3-gaps xorg-xinit xorg-apps xterm dmenu xdm-archlinux i3status git firefox numlockx ark mc htop conky polkit network-manager-applet dolphin ntfs-3g dosfstools qt5ct lxappearance-gtk3 papirus-icon-theme picom redshift tint2 grc flameshot xscreensaver notification-daemon adwaita-qt5 gnome-themes-extra alsa-utils alsa-plugins lib32-alsa-plugins alsa-firmware alsa-card-profiles pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol freetype2 noto-fonts-extra noto-fonts-cjk cheese kwrite mesa lib32-mesa go wireless_tools packagekit-qt5 winetricks wine geoclue2  --noconfirm
