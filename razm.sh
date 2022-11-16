702f1054d9f76d00bb385326576ee7202b563e5f
stardestart <99838873+stardestart@users.noreply.github.com>
2022-11-16 22:53:22 +0300

    Update razm.sh

--- razm.sh
+++ razm.sh
@@ -25,26 +25,26 @@ n
 t
 2
 4
-n
-3
-
-+1g
-n
-4
-
-
-w
-EOF
+#n
+#3
+#
+#+1g
+#n
+#4
+#
+#
+#w
+#EOF
 mkfs.ext2 /dev/${disk}1 -L boot<<EOF
 y
 EOF
-mkswap /dev/${disk}3 -L swap
-mkfs.ext4 /dev/${disk}4 -L root<<EOF
-y
-EOF
-mount /dev/${disk}4 /mnt
-mkdir /mnt/boot
-mount /dev/${disk}1 /mnt/boot
+#mkswap /dev/${disk}3 -L swap
+#mkfs.ext4 /dev/${disk}4 -L root<<EOF
+#y
+#EOF
+#mount /dev/${disk}4 /mnt
+#mkdir /mnt/boot
+#mount /dev/${disk}1 /mnt/boot
 else
 fdisk /dev/$disk <<EOF
 g
