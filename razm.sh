23e7a6bb1fca2583263151b1864e0cd1ed8c45e3
stardestart <99838873+stardestart@users.noreply.github.com>
2022-11-17 11:36:38 +0300

    Update razm.sh

--- razm.sh
+++ razm.sh
@@ -11,67 +11,67 @@ echo "
 "
 swapoff -a
 umount -R /mnt
-#boot="$(efibootmgr | grep Boot)"
-#if [ -z "$boot" ];
-#then
-#fdisk /dev/$disk<<EOF
-#g
-#n
-#1
-#2048
-#+512m
-#n
-#2
-#\n
-#+1m
-#t
-#2
-#4
-#n
-#3
-#\n
-#+1g
-#n
-#4
-#\n
-#\n
-#w
-#EOF
-#mkfs.ext2 /dev/${disk}1 -L boot<<EOF
-#y
-#EOF
-#mkswap /dev/${disk}3 -L swap
-#mkfs.ext4 /dev/${disk}4 -L root<<EOF
-#y
-#EOF
-#mount /dev/${disk}4 /mnt
-#mount --mkdir /dev/${disk}1 /mnt/boot
-#else
-#fdisk /dev/$disk<<EOF
-#g
-#n
-#1
-#2048
-#+512m
-#t
-#1
-#n
-#2
-#\n
-#+1g
-#n
-#3
-#\n
-#\n
-#w
-#EOF
-#mkfs.fat -F32 /dev/${disk}1 -n boot<<EOF
-#y
-#EOF
-#mkswap /dev/${disk}2 -L swap
-#mkfs.ext4 /dev/${disk}3 -L root<<EOF
-#y
-#EOF
-#mount /dev/${disk}3 /mnt
-#mount --mkdir /dev/${disk}1 /mnt/boot
-#fi
+boot="$(efibootmgr | grep Boot)"
+if [ -z "$boot" ];
+then
+fdisk /dev/$disk<<EOF
+g
+n
+1
+2048
++512m
+n
+2
+\n
++1m
+t
+2
+4
+n
+3
+\n
++1g
+n
+4
+\n
+\n
+w
+EOF
+mkfs.ext2 /dev/${disk}1 -L boot<<EOF
+y
+EOF
+mkswap /dev/${disk}3 -L swap
+mkfs.ext4 /dev/${disk}4 -L root<<EOF
+y
+EOF
+mount /dev/${disk}4 /mnt
+mount --mkdir /dev/${disk}1 /mnt/boot
+else
+fdisk /dev/$disk<<EOF
+g
+n
+1
+2048
++512m
+t
+1
+n
+2
+\n
++1g
+n
+3
+\n
+\n
+w
+EOF
+mkfs.fat -F32 /dev/${disk}1 -n boot<<EOF
+y
+EOF
+mkswap /dev/${disk}2 -L swap
+mkfs.ext4 /dev/${disk}3 -L root<<EOF
+y
+EOF
+mount /dev/${disk}3 /mnt
+mount --mkdir /dev/${disk}1 /mnt/boot
+fi
