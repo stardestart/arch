e511f62ca13d2b1fcc390a0c45e16cd1cd823c7f
Alex New <stardestart@icloud.com>
2022-11-24 23:02:28 +0300

    Rev_18

--- 1.sh
+++ 1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 username="virtual"
 sysdisk="sda"
-massdisks=$(lsblk -sno Name | grep -ivE "└─|$sysdisk|rom|usb")
+massdisks=$(lsblk -sn | grep -ivE "└─|$sysdisk|rom|usb|/" | awk '{print $1}')
 echo -e $massdisks
 for (( j=0, i=1; i<="${#massdisks[*]}"; i++, j++ ))
 do
 if [ -z $(lsblk -no LABEL /dev/${massdisks[$j]}) ];
 then
 echo ${massdisks[$j]}
-mount --mkdir /mnt/dev/${massdisks[$j]} /mnt/home/$username/${massdisks[$j]}/
+arch-chroot /mnt mount --mkdir /dev/${massdisks[$j]} /home/$username/${massdisks[$j]}/
 else
 echo ${massdisks[$j]}
 lsblk -no LABEL /dev/${massdisks[$j]}
-mount --mkdir /mnt/dev/$(lsblk -no LABEL /dev/${massdisks[$j]}) /mnt/home/$username/$(lsblk -no LABEL /dev/${massdisks[$j]})/
+arch-chroot /mnt mount --mkdir /dev/$(lsblk -no LABEL /dev/${massdisks[$j]}) /home/$username/$(lsblk -no LABEL /dev/${massdisks[$j]})/
 fi
 done
