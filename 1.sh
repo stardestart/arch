b7b54ad9160079ca357d8f9373b8a682789be817
Alex New <stardestart@icloud.com>
2022-11-24 22:22:58 +0300

    Rev_16

---
+++ 1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+username="virtual"
+sysdisk="sda"
+massdisks=$(lsblk -sno Name | grep -ivE "└─|$sysdisk")
+echo -e $massdisks
+for (( j=0, i=1; i<="${#massdisks[*]}"; i++, j++ ))
+do
+if [ -z $(lsblk -no LABEL /dev/${massdisks[$j]}) ];
+then
+echo ${massdisks[$j]}
+mount --mkdir /mnt/dev/${massdisks[$j]} /home/$username/${massdisks[$j]}/
+else
+echo ${massdisks[$j]}
+lsblk -no LABEL /dev/${massdisks[$j]}
+mount --mkdir /mnt/dev/$(lsblk -no LABEL /dev/${massdisks[$j]}) /home/$username/$(lsblk -no LABEL /dev/${massdisks[$j]})/
+fi
+done
