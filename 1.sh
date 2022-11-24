3fa4331202e13582df600009bb2a7447326b3c21
Alex New <stardestart@icloud.com>
2022-11-24 23:18:45 +0300

    Rev_20

--- 1.sh
+++ 1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 username="virtual"
 sysdisk="sda"
-massdisks=$(lsblk -sn | grep -ivE "└─|$sysdisk|rom|usb|/" | awk '{print $1}')
+massdisks="$(lsblk -sn | grep -ivE "└─|$sysdisk|rom|usb|/" | awk '{print $1}')"
 echo -e $massdisks
 for (( j=0, i=1; i<="${#massdisks[*]}"; i++, j++ ))
 do
-if [ -z $(lsblk -no LABEL /dev/${massdisks[$j]}) ];
+if [ -z "$(lsblk -no LABEL /dev/"${massdisks[$j]}")" ];
 then
-echo ${massdisks[$j]}
-arch-chroot /mnt mount --mkdir /dev/${massdisks[$j]} /home/$username/${massdisks[$j]}/
+echo "${massdisks[$j]}"
+arch-chroot /mnt mount --mkdir /dev/"${massdisks[$j]}" /home/"$username"/"${massdisks[$j]}"/
 else
-echo ${massdisks[$j]}
-lsblk -no LABEL /dev/${massdisks[$j]}
-arch-chroot /mnt mount --mkdir /dev/${massdisks[$j]} /home/$username/$(lsblk -no LABEL /dev/${massdisks[$j]})/
+echo "${massdisks[$j]}"
+lsblk -no LABEL /dev/"${massdisks[$j]}"
+arch-chroot /mnt mount --mkdir /dev/"${massdisks[$j]}" /home/"$username"/"$(lsblk -no LABEL /dev/"${massdisks[$j]}")"/
 fi
 done
