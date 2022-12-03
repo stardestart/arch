ed07a8453f1098df78ded4d65954efa61fed688b
Alex New <stardestart@icloud.com>
2022-12-03 20:24:13 +0300

    Rev_151

--- test.sh
+++ test.sh
@@ -254,10 +254,10 @@ fi
 #
 #Установка и настройка программы для фильтрования зеркал и обновление ключей.
 echo -e "\033[31mУстановка и настройка программы для фильтрования зеркал и обновление ключей.\033[32m"
-pacman-key --init
-pacman-key --populate archlinux
 pacman -Sy archlinux-keyring reflector --noconfirm
 reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman-key --init
+pacman-key --populate archlinux
 #
 #Установка ОС.
 echo -e "\033[31mУстановка ОС.\033[32m"
