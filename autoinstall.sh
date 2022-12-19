9cc41866ace46349205955980b060e8185d10c0e
stardestart <99838873+stardestart@users.noreply.github.com>
2022-12-19 08:53:29 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -406,7 +406,7 @@ ${color #b2b2b2}Объём:$alignr${fs_size /home/'"$username"'/'"$(lsblk -no LA
 #
 #Копирование файла автоматического монтирования разделов.
 echo -e "\033[36mПеренос genfstab.\033[0m"
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt > /mnt/etc/fstab
 #
 #Создание общего конфига загрузки оконного менеджера.
 echo -e "\033[36mСоздание xinit.\033[0m"
@@ -437,7 +437,7 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
 fi
 xhost +si:localuser:root #Позволяет пользователю root получить доступ к работающему X-серверу.
 feh --bg-max --randomize --no-fehbg /usr/share/backgrounds/archlinux/ & #Автозапуск обоев рабочего стола.
-exec i3 #Автозапуск i3.' > /mnt/etc/X11/xinit/xinitrc
+exec i3 #Автозапуск i3.' > /mnt/home/"$username"/xinitrc
 #
 #Создание общего конфига клавиатуры.
 echo -e "\033[36mСоздание 00-keyboard.\033[0m"
