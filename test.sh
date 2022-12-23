3fbb0cb734a610c2b5b3777fb054cdd6346fe197
stardestart <99838873+stardestart@users.noreply.github.com>
2022-12-23 08:39:03 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -996,7 +996,7 @@ bar {
             # Цвет границы, фона и текста для кнопки активного рабочего стола.
             focused_workspace  #4c7899 #285577 #f92b2b
             # Цвет границы, фона и текста для кнопки не активного рабочего стола.
-            inactive_workspace #333333 #222222 #b2f9b2
+            inactive_workspace #333333 #222222 #2bf92b
             }
          # Сделайте снимок экрана, щелкнув правой кнопкой мыши на панели (--no-startup-id убирает курсор загрузки).
          bindsym --release button3 exec --no-startup-id import ~/latest-screenshot.png
