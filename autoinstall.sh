94bd478a71c7f99ea4f8be7b504d5371a15c1913
stardestart <99838873+stardestart@users.noreply.github.com>
2022-12-24 11:40:54 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1365,8 +1365,8 @@ user_pref("accessibility.typeaheadfind", true);
 user_pref("intl.regional_prefs.use_os_locales", true);
 user_pref("widget.gtk.overlay-scrollbars.enabled", false);
 user_pref("browser.startup.page", 3);
-user_pref("browser.download.useDownloadDir", false);\047 > $_/user.js
-sed -i \047s/Google/DuckDuckGo/\047 $_/user.js
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");\047 > $_/user.js
 if [ -n "$(clinfo -l)" ];
     then sed -i \047s/#TechnicalSymbol //\047 ~/.config/picom.conf
     else sed -i \047/#TechnicalSymbol /d\047 ~/.config/picom.conf
