7b180a30cabf01b7ea77b5c2a28f40591a4087ce
stardestart <99838873+stardestart@users.noreply.github.com>
2023-01-07 10:26:51 +0300

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -1045,6 +1045,7 @@ bar {
          # Сделайте снимок экрана, щелкнув правой кнопкой мыши на панели (--no-startup-id убирает курсор загрузки).
          bindsym --release button3 exec --no-startup-id import ~/latest-screenshot.png
 }
+exec --no-startup-id firefox; #TechnicalString
 exec --no-startup-id ~/archinstall.sh; #TechnicalString' > /mnt/home/"$username"/.config/i3/config
 #
 #Создание конфига i3status.
