94e385e6bb5d6a7edbdb5101e334457069a9477c
stardestart <99838873+stardestart@users.noreply.github.com>
2023-01-08 20:55:56 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -270,11 +270,9 @@ fi
 #
 #Установка и настройка программы для фильтрования зеркал и обновление ключей.
 echo -e "\033[36mУстановка и настройка программы для фильтрования зеркал и обновление ключей.\033[0m"
-pacman --color always -Sy archlinux-keyring --noconfirm
 pacman-key --init
 pacman-key --populate archlinux
-pacman --color always -Sy gnupg --noconfirm
-pacman --color always -Sy reflector --noconfirm
+pacman --color always -Sy archlinux-keyring gnupg reflector --noconfirm
 reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 #
 #Установка ОС.
