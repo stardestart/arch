839314f8c8ebb5a51a8ff04e363b981d5c25ad2a
stardestart <99838873+stardestart@users.noreply.github.com>
2023-01-29 17:37:40 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
-pacman -S openvpn strongswan --noconfirm
+pacman -S openvpn strongswan iptables --noconfirm
 rm /etc/ipsec.conf
 mkdir -p ~/cert/{cacerts,certs,private}
 pki --gen --type rsa --size 4096 --outform pem > ~/cert/private/ca-key.pem
@@ -44,4 +44,19 @@ systemctl start strongswan-starter
 systemctl enable strongswan-starter
 systemctl restart strongswan-starter
 rm -rf ~/cert
-rm -rf ~/pki
+echo 'net.ipv4.ip_forward = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.ip_no_pmtu_disc = 1' > /etc/sysctl.conf
+sysctl -p
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j sudo ACCEPT iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p udp --dport 500 -j ACCEPT iptables -A INPUT -p udp --dport 4500 -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir in --proto esp -s 10.10.10.0/24 -j ACCEPT iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+netfilter-persistent save 
+netfilter-persistent reload
+systemctl stop iptables
+systemctl start iptables
+iptables -S
