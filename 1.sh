94d2ca4aaefd0c322fad5749b5d07cf49461f2c3
stardestart <99838873+stardestart@users.noreply.github.com>
2023-01-29 14:35:58 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
-pacman -S openvpn strongswan ufw --noconfirm
-rm /etc/ipsec.conf
+pacman -S openvpn strongswan --noconfirm
+pacman -Rs ufw --noconfirm
+rm /etc/ipsec.conf /etc/ufw/sysctl.conf /etc/ufw/before.rules
 mkdir -p ~/pki/{cacerts,certs,private}
 chmod 700 ~/pki
 pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
@@ -43,35 +44,10 @@ conn ikev2-vpn
     eap_identity=%identity
     ike=chacha20poly1305-sha512-curve25519-prfsha512,aes256gcm16-sha384-prfsha384-ecp384,aes256-sha1-modp1024,aes128-sha1-modp1024,3des-sha1-modp1024!
     esp=chacha20poly1305-sha512,aes256gcm16-ecp384,aes256-sha256,aes256-sha1,3des-sha1!' > /etc/ipsec.conf
-echo ': RSA "server-key.pem"
+echo '1217581-cn58105.tw1.ru : RSA "server-key.pem"
 stardestart : EAP "@DeFeNdEr1410@"
 alisa : EAP "@DeFeNdEr1410@"
 misha : EAP "mishavpn2023"
 ruslan : EAP "ruslan2023"' > /etc/ipsec.secrets
 systemctl start strongswan-starter
 systemctl enable strongswan-starter
-ufw allow OpenSSH
-ufw enable
-ufw allow 500,4500/udp
-echo '*nat
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
-COMMIT
-
-*mangle
--A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-COMMIT
-
-*filter
-:ufw-before-input - [0:0]
-:ufw-before-output - [0:0]
-:ufw-before-forward - [0:0]
-:ufw-not-local - [0:0]
--A ufw-before-forward --match policy --pol ipsec --dir in --proto esp -s 10.10.10.0/24 -j ACCEPT
--A ufw-before-forward --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT' > /etc/ufw/before.rules
-echo 'net/ipv4/ip_forward=1
-net/ipv4/conf/all/accept_redirects=0
-net/ipv4/conf/all/send_redirects=0
-net/ipv4/ip_no_pmtu_disc=1' > /etc/ufw/sysctl.conf
-ufw disable
-ufw enable
