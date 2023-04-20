0b31aa83e561332124209287c35fd01605cba644
stardestart <stardestart@icloud.com>
2023-04-20 20:34:26 +0300

    Rev_55

--- test.sh
+++ test.sh
@@ -272,7 +272,7 @@ fi
 echo -e "\033[36mУстановка и настройка программы для фильтрования зеркал и обновление ключей.\033[0m"
 pacman-key --init
 pacman-key --populate archlinux
-pacman-key —refresh-keys
+pacman-key --refresh-keys
 pacman --color always -Syy archlinux-keyring gnupg reflector --noconfirm
 reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 #
