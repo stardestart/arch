48915f3ca6086426e07a05338fc7c6a4c66ba2a3
stardestart <99838873+stardestart@users.noreply.github.com>
2023-05-04 15:10:07 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -395,11 +395,12 @@ if [ -z "$(efibootmgr | grep Boot)" ];
         arch-chroot /mnt sed -i 's/#GRUB_DISABLE_RECOVERY=true/GRUB_DISABLE_RECOVERY=true/' /etc/default/grub
         arch-chroot /mnt sed -i 's/GRUB_DISABLE_LINUX_UUID=true/#GRUB_DISABLE_LINUX_UUID=true/' /etc/default/grub
         arch-chroot /mnt sed -i 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="resume=\/dev\/'"$sysdisk"''"$p3"' /' /etc/default/grub
-        grubsha="$(sh -c 'grub-mkpasswd-pbkdf2 << EOF
+        grubsha=$(grub-mkpasswd-pbkdf2 << EOF
         $passuser
         $passuser
-        EOF')"
-        grubsha="$(echo $x | awk '{print $NF}')"
+        EOF
+        )
+        grubsha="$(echo $grubsha | awk '{print $NF}')"
         arch-chroot /mnt sed -i 's/CLASS="--class gnu-linux --class gnu --class os"/CLASS="--class gnu-linux --class gnu --class os --unrestricted"/' /etc/grub.d/10_linux
         echo 'cat << EOF
         set superusers='"$username"'
