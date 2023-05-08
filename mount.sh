2c07972ac2796fca51978f087b27951bf0661ce7
stardestart <99838873+stardestart@users.noreply.github.com>
2023-05-08 07:56:06 +0500

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -4,4 +4,4 @@ mount --mkdir /dev/sda1 /mnt/boot
 mount --mkdir /dev/sda5 /mnt/var
 mount --mkdir /dev/sda6 /mnt/home
 swapon /dev/sda3
-arch-chroot /mnt nmcli device wifi connect "$(find /var/lib/iwd -type f -name "*.psk" -printf "%f" | sed s/.psk//)" password "$(cat /var/lib/iwd/"$(sudo find /var/lib/iwd -type f -name "*.psk" -printf "%f")" | grep --color=never Passphrase= | sed s/Passphrase=//)"
+arch-chroot /mnt nmcli device wifi connect \047"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f" | sed s/.psk//)"\047 password "$(cat /var/lib/iwd/"$(sudo find /var/lib/iwd -type f -name "*.psk" -printf "%f")" | grep --color=never Passphrase= | sed s/Passphrase=//)"
