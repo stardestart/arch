6dd2e1766783a1480f93bd9364d6e1009af30ee5
stardestart <99838873+stardestart@users.noreply.github.com>
2023-05-12 11:17:37 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -204,7 +204,7 @@ varsize=$(bc << EOF
 $rootsize/2
 EOF
 )
-#if [ $rootsize -lt 20 ]; then rootsize=20
+if [ $rootsize -lt 20 ]; then rootsize=20; fi
 varsize="$varsize"G
 echo -e "\033[36mРазмер var-раздела: $varsize\033[0m"
 rootsize="$rootsize"G
