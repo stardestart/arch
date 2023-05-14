3db97fde57cc4e1744dba85a4c5006edfc001182
stardestart <99838873+stardestart@users.noreply.github.com>
2023-05-14 18:47:00 +0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mount /dev/sda4 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
-mount --mkdir /dev/sda5 /mnt/var
-mount --mkdir /dev/sda6 /mnt/home
-swapon /dev/sda3
+#mount /dev/sda4 /mnt
+#mount --mkdir /dev/sda1 /mnt/boot
+#mount --mkdir /dev/sda5 /mnt/var
+#mount --mkdir /dev/sda6 /mnt/home
+#swapon /dev/sda3
 echo -e '#!/bin/bash
-nmcli device wifi connect \047'"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f" | sed s/.psk//)"'\047 password \047'"$(cat /var/lib/iwd/"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f")" | grep --color=never Passphrase= | sed s/Passphrase=//)"'\047' > /mnt/home/stardestart/archinstall.sh
+nmcli device wifi connect \047'"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f" | sed s/.psk//)"'\047 password \047'"$(grep --color=never Passphrase= /var/lib/iwd/"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f")" | sed s/Passphrase=//"'\047' > /mnt/home/stardestart/archinstall.sh
