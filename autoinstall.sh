d9faf06b8bfef70753453be98a9044cf789e4367
stardestart <99838873+stardestart@users.noreply.github.com>
2023-07-07 23:37:05 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1631,7 +1631,7 @@ echo -e '#!/bin/bash
 sleep 10
 nmcli device wifi connect "'"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f" | sed s/.psk//)"'" password "'"$(grep Passphrase= /var/lib/iwd/"$(find /var/lib/iwd -type f -name "*.psk" -printf "%f")" | sed s/Passphrase=//)"'"
 echo -e "\033[36mЗавершение установки.\033[0m" > /dev/pts/0
-while [[ -z "$(xlsclients -al | grep firefox)" ]]; do
+while [[ -z "$(xwininfo -root -tree | grep -i firefox | grep -i mozilla)" ]]; do
     echo "\033[31mОжидание запуска браузера!\033[0m" > /dev/pts/0
     sleep 5
 done
