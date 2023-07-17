622701b2cbdcacc48dc4b467a18458499780a482
stardestart <99838873+stardestart@users.noreply.github.com>
2023-07-17 14:38:58 +0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -4,4 +4,5 @@ mount --mkdir /dev/nvme0n1p1 /mnt/boot
 mount --mkdir /dev/nvme0n1p4 /mnt/var
 mount --mkdir /dev/nvme0n1p5 /mnt/home
 swapon /dev/nvme0n1p2
-arch-chroot /mnt bootctl install
+arch-chroot /mnt pacman -Sy linux-zen
+arch-chroot /mnt mkinitcpio
