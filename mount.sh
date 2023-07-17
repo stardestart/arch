2617954b5f8b79dcb7fc7409273445f9030e189b
stardestart <stardestart@icloud.com>
2023-07-17 16:42:18 +0300

    Rev_76

--- mount.sh
+++ mount.sh
@@ -4,5 +4,4 @@ mount --mkdir /dev/nvme0n1p1 /mnt/boot
 mount --mkdir /dev/nvme0n1p4 /mnt/var
 mount --mkdir /dev/nvme0n1p5 /mnt/home
 swapon /dev/nvme0n1p2
-arch-chroot /mnt pacman -Sy linux-zen
-arch-chroot /mnt mkinitcpio
+#arch-chroot /mnt pacman -Sy linux-zen
