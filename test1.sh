bf3b5bcd148df6eefe281f0bcc44b2a740bc1c9a
stardestart <stardestart@icloud.com>
2024-07-03 20:04:17 +0300

    Rev_118

--- test1.sh
+++ test1.sh
@@ -8,11 +8,9 @@ gpg-connect-agent reloadagent /bye
 rm /var/lib/pacman/db.lck
 rm -R /root/.gnupg/
 rm -R /etc/pacman.d/gnupg/
+mount -o remount,size=2G /run/archiso/cowspace
 sed -i '/= Required DatabaseOptional/c\SigLevel = Required DatabaseOptional TrustAll' /etc/pacman.conf
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy glibc --noconfirm
 pacman -Sy lib32-glibc --noconfirm
-
-fdisk -l
-lsblk -l
