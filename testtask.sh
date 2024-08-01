ede600bf7b9d8389c9f6d8908296c6720184e440
stardestart <stardestart@icloud.com>
2024-08-01 20:24:15 +0300

    Rev_149

--- testtask.sh
+++ testtask.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
 #apt install docker-ce -y
-apt install curl software-properties-common ca-certificates apt-transport-https -y
-wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-cache policy docker-ce
-apt install docker-ce -y
+apt-get install docker-compose-plugin
 #
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #
