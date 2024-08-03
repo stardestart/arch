27bc9e6c725e360dbe525ebee971ae9465516578
stardestart <stardestart@icloud.com>
2024-08-03 19:18:47 +0300

    Rev_166

--- testtask.sh
+++ testtask.sh
@@ -6,43 +6,44 @@
 #
 #sudo snap install cqlsh
 #
-echo 'networks:
-  cassandra-net:
-    driver: bridge
-    ipam:
-      config:
-        - subnet: 192.168.1.0/24
-
+echo '# Определение сервисов
 services:
+  # Сервис для узла Cassandra 1
   cass-db-1:
+    # Использование последнего образа Cassandra
     image: cassandra:latest
+    # Установка имени контейнера
     container_name: cass-db-1
+    # Пропуск порта 9042 на хосте к порту 9042 в контейнере (для CQLSH)
     ports:
-      - "9042:9042" # for cqlsh
-    networks:
-      cassandra-net:
-        ipv4_address: 192.168.1.200
+      - "9042:9042"
+    # Использование режима сети host
+    network_mode: "host"
 
+  # Сервис для узла Cassandra 2
   cass-db-2:
+    # Установка имени контейнера
     container_name: cass-db-2
+    # Использование последнего образа Cassandra
     image: cassandra:latest
+    # Пропуск порта 9043 на хосте к порту 9042 в контейнере (для CQLSH)
     ports:
-      - "9043:9042" # for cqlsh
-    networks:
-      cassandra-net:
-        ipv4_address: 192.168.1.201
-    depends_on:
-      - cass-db-1
+      - "9043:9042"
 
+    # Использование режима сети host
+    network_mode: "host"
+
+  # Сервис для узла Cassandra 3
   cass-db-3:
+    # Установка имени контейнера
     container_name: cass-db-3
+    # Использование последнего образа Cassandra
     image: cassandra:latest
+    # Пропуск порта 9044 на хосте к порту 9042 в контейнере (для CQLSH)
     ports:
-      - "9044:9042" # for cqlsh
-    networks:
-      cassandra-net:
-        ipv4_address: 192.168.1.202
-    depends_on:
-      - cass-db-2' > docker-compose.yml
+      - "9044:9042"
+
+    # Использование режима сети host
+    network_mode: "host"' > docker-compose.yml
 #
 #sudo docker-compose up
