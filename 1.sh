fec1baa2fdf064520483fd9bba8e341ec73a58ed
stardestart <stardestart@icloud.com>
2024-08-04 11:10:55 +0300

    Rev_208

--- 1.sh
+++ 1.sh
@@ -1,2 +1,3 @@
-sudo docker ps -a
-sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra-1
+#sudo docker ps -a
+#sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra-1
+sudo ip route add 172.16.1.0/24 via 172.16.1.200
