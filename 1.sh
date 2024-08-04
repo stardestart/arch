9930e205a1a271ce3d961ea4e82fce8d0b3b81ad
stardestart <stardestart@icloud.com>
2024-08-04 12:24:49 +0300

    Rev_214

--- 1.sh
+++ 1.sh
@@ -1,3 +1,8 @@
 #sudo docker ps -a
-sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra-1
+#sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra-1
 #sudo ip route add 172.16.1.0/24 via 172.16.1.200
+echo "Test text" | mail -s "Test title" stardestart@gmail.com
+sudo docker-compose logs cassandra-1 (или cassandra-2, cassandra-3 и т.д.)
+sudo docker-compose ps
+sudo journalctl -u docker
+sudo journalctl -b
