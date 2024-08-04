ba474ded235cafd3671ddf5e0d7ae160ddf48954
stardestart <stardestart@icloud.com>
2024-08-04 16:07:14 +0300

    Rev_219

--- testtask.sh
+++ testtask.sh
@@ -1,60 +1,49 @@
 #!/bin/bash
 
 sudo apt install docker-compose -y
-# Установка docker-compose с помощью apt, флаг -y указывает на автоматическое подтверждение установки
+sudo apt install snapd
+#
+sudo snap install cqlsh
 
 echo '---
 networks:
-  host-network:
-    name: host-network
-    driver: bridge
+  cassandra-net:
+    name: cassandra-net
+    driver: macvlan
     ipam:
      config:
-       - subnet: 192.168.1.0/24
-# Определение сети host-network с диапазоном адресов 192.168.1.0/24
+       - subnet: 172.16.1.0/24
 
 services:
-  cass-db-seed:
-    image: cassandra:5
-    container_name: cass-db-seed
-    ports:
-      - 9042:9042
-    networks:
-      host-network:
-        ipv4_address: 192.168.1.200
+  cassandra-1:
+    image: cassandra:latest
+    container_name: cassandra-1
     restart: always
-# Определение сервиса cass-db-seed, который будет использовать образ cassandra:5, порт 9042 и адрес 192.168.1.200
+    ports:
+      - "9042:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 172.16.1.200
 
-  cass-db-1:
-    container_name: cass-db-1
-    image: cassandra:5
-    ports:
-      - 9043:9042
-    environment:
-      - CASSANDRA_SEEDS=cass-db-seed
-    networks:
-      host-network:
-        ipv4_address: 192.168.1.201
-    depends_on:
-      - cass-db-seed
+  cassandra-2:
+    image: cassandra:latest
+    container_name: cassandra-2
     restart: always
-# Определение сервиса cass-db-1, который будет использовать образ cassandra:5, порт 9043, адрес 192.168.1.201 и зависеть от сервиса cass-db-seed
+    ports:
+      - "9043:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 172.16.1.201
 
-  cass-db-2:
-    container_name: cass-db-2
-    image: cassandra:5
-    ports:
-      - 9044:9042
-    environment:
-      - CASSANDRA_SEEDS=cass-db-seed
-    networks:
-      host-network:
-        ipv4_address: 192.168.1.202
-    depends_on:
-      - cass-db-seed
+  cassandra-3:
+    image: cassandra:latest
+    container_name: cassandra-3
     restart: always
-# Определение сервиса cass-db-2, который будет использовать образ cassandra:5, порт 9044, адрес 192.168.1.202 и зависеть от сервиса cass-db-seed
-' > docker-compose.yml
+    ports:
+      - "9044:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 172.16.1.202' > docker-compose.yml
 
 sudo docker-compose up -d
 # Запуск docker-compose в фоновом режиме
