8fa2aade989e2cf60ccc9b5e3d39ef0310d187f0
stardestart <stardestart@icloud.com>
2024-08-05 22:44:27 +0300

    Rev_238

--- 1.sh
+++ 1.sh
@@ -2,6 +2,7 @@ sudo ip link set lo down
 sudo brctl addbr br0
 sudo brctl addif br0 enp0s3
 sudo brctl addif br0 br-77cf37c99627
+sudo ip link up br0
 #
 #sudo iptables -A FORWARD -i br-4d9b06be0aee -o enp0s3 -j ACCEPT
 #sudo iptables -A FORWARD -i enp0s3 -o br-4d9b06be0aee -j ACCEPT
