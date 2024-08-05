0a464621c6e64f9b0916d95922867fc2c45ab47a
stardestart <stardestart@icloud.com>
2024-08-05 20:56:19 +0300

    Rev_229

--- 1.sh
+++ 1.sh
@@ -1,2 +1,5 @@
 sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 ! -o br-4cc67dcd34a0 -j MASQUERADE
-iptables-save > /etc/sysconfig/iptables
+sudo iptables -A FORWARD -i br-4cc67dcd34a0 -o enp0s3 -j ACCEPT
+sudo iptables -A FORWARD -i enp0s3 -o br-4cc67dcd34a0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o enp0s3 -j MASQUERADE
+sudo sysctl -w net.ipv4.ip_forward=1
