7a60aa5bede54c7d022ee5b81fb89abe7a2f2b81
stardestart <stardestart@icloud.com>
2024-08-06 18:59:33 +0300

    Rev_249

--- 1.sh
+++ 1.sh
@@ -1,2 +1,3 @@
 sudo docker network create -d ipvlan --subnet=192.168.1.0/24 --gateway=192.168.1.254 -o parent=docker0 cassandra-net
+sudo ip addr add 192.168.1.0/24 dev docker0
 #sudo docker run -d --name cassandra-1 --restart always -p 9042:9042 --net cassandra-net --ip 192.168.1.200 cassandra:latest
