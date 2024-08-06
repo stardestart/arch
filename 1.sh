f63f1a3a1bf285002d72ff351e274da82e6a9420
stardestart <stardestart@icloud.com>
2024-08-06 21:44:03 +0300

    Rev_264

--- 1.sh
+++ 1.sh
@@ -1,38 +1,45 @@
+#!/bin/bash
 
-#sudo ip link add vlan0 link eth0 type vlan id 10
-#sudo ip link set vlan0 up
-#sudo ip addr add 192.168.1.100/24 dev vlan0
-#sudo docker network create -d ipvlan --subnet=192.168.1.0/24 --gateway=192.168.1.254 -o parent=vlan0 cassandra-net
+sudo apt install docker-compose -y
 
-#sudo docker run -d --name cassandra-1 --restart always -p 9042:9042 --net cassandra-net --ip 192.168.1.200 cassandra:latest
+echo '---
+services:
+  cassandra-1:
+    image: cassandra:latest
+    restart: always
+    ports:
+      - "9042:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 192.168.1.200
 
-#sudo brctl addbr br0
-#sudo brctl addif br0 vlan0
-#sudo brctl addif br0 enp0s3
-#sudo ip link set br0 up
-#sudo ip route add default via 192.168.1.1 dev br0
-#&&sudo ip route add 192.168.1.0/24 via 192.168.1.254 dev br0
-#sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o enp0s3 -j MASQUERADE
-#sudo brctl addbr br0
-#sudo brctl addif br0 enp0s3
-#sudo ip addr add 192.168.1.1/24 dev enp0s3
-#sudo docker network create -d bridge --attachable --subnet=192.168.1.0/24 --gateway=192.168.1.254 cassandra-net
-#sudo ip addr add 192.168.1.254/24 dev br0
-#sudo ip route add default via 192.168.1.1 dev br0
-#sudo systemctl restart docker
+  cassandra-2:
+    image: cassandra:latest
+    restart: always
+    ports:
+      - "9043:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 192.168.1.201
 
-#sudo ip link add br0 type bridge
-#sudo ip link set enp0s3 master br0
-#sudo ip addr add 192.168.1.66/24 dev br0
-#sudo docker network create -d bridge --attachable --subnet=192.168.1.0/24 --gateway=192.168.1.66 -o "com.docker.network.bridge.name"="br0" cassandra-net
-#sudo docker network connect cassandra-net br0
-#sudo sysctl -w net.ipv4.ip_forward=1
-#sudo systemctl restart docker
+  cassandra-3:
+    image: cassandra:latest
+    restart: always
+    ports:
+      - "9044:9042"
+    networks:
+      cassandra-net:
+        ipv4_address: 192.168.1.202
 
+networks:
+  cassandra-net:
+    driver: ipvlan
+    driver_opts:
+      parent: enp0s3
+    ipam:
+      driver: default
+      config:
+        - subnet: 192.168.1.0/24
+          gateway: 192.168.1.66' > docker-compose.yml
 
-
-sudo docker network create -d ipvlan --subnet=192.168.1.0/24 --gateway=192.168.1.66 -o parent=enp0s3 cassandra-net
-sudo ip link add cassandra-net link enp0s3 type ipvlan mode l2
-#sudo ip addr add 192.168.0.223/32 dev cassandra-net
-sudo ip link set cassandra-net up
-#sudo ip route add 192.168.0.192/27 dev cassandra-net
+sudo docker-compose up -d
