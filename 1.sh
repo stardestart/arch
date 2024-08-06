13998ae1330c78cf8204c7bd91b173e5ce91ed63
stardestart <stardestart@icloud.com>
2024-08-06 19:23:31 +0300

    Rev_255

--- 1.sh
+++ 1.sh
@@ -1,9 +1,11 @@
-sudo ip addr add 192.168.1.100/24 dev eth0.0
-sudo docker network rm cassandra-net
-sudo docker network create -d ipvlan --subnet=192.168.1.0/24 --gateway=192.168.1.254 -o parent=eth0.0 cassandra-net
+
+sudo ip link add vlan0 link eth0 type vlan id 10
+sudo ip link set vlan0 up
+sudo ip addr add 192.168.1.100/24 dev vlan0
+sudo docker network create -d ipvlan --subnet=192.168.1.0/24 --gateway=192.168.1.254 -o parent=vlan0 cassandra-net
 #sudo docker run -d --name cassandra-1 --restart always -p 9042:9042 --net cassandra-net --ip 192.168.1.200 cassandra:latest
 sudo brctl addbr br0
-sudo brctl addif br0 eth0.0
+sudo brctl addif br0 vlan0
 sudo brctl addif br0 enp0s3
 sudo ip link set br0 up
 sudo ip route add 192.168.1.0/24 via 192.168.1.254 dev br0
