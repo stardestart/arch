9fceed974910694bb8da662ec9bb43eb59ef3428
stardestart <stardestart@icloud.com>
2024-08-06 21:22:14 +0300

    Rev_263

--- testtask1_2.sh
+++ testtask1_2.sh
@@ -10,57 +10,13 @@ sudo apt install docker-compose -y
 
 #sudo snap install cqlsh
 
-
-# Создание моста
-
-sudo brctl addbr br0
-
-
-# Добавление интерфейса к мосту
-
-sudo brctl addif br0 eth0
-
-
-# Создание виртуального интерфейса
-
-sudo ip link add eth0.01 link eth0 type vlan id 1
-
-
-# Настройка моста
-
-sudo ip link set br0 up
-
-
 echo '---
-
-networks:
-
-  cassandra-net:
-
-    name: cassandra-net
-
-    driver: macvlan
-
-    driver_opts:
-
-      parent: br0
-
-    ipam:
-
-     config:
-
-       - subnet: 192.168.1.0/24
-
-         gateway: 192.168.1.254
-
 
 services:
 
   cassandra-1:
 
     image: cassandra:latest
-
-    container_name: cassandra-1
 
     restart: always
 
@@ -79,8 +35,6 @@ services:
 
     image: cassandra:latest
 
-    container_name: cassandra-2
-
     restart: always
 
     ports:
@@ -98,8 +52,6 @@ services:
 
     image: cassandra:latest
 
-    container_name: cassandra-3
-
     restart: always
 
     ports:
@@ -110,13 +62,34 @@ services:
 
       cassandra-net:
 
-        ipv4_address: 192.168.1.202' > docker-compose.yml
+        ipv4_address: 192.168.1.202
+
+
+networks:
+
+  cassandra-net:
+
+    driver: ipvlan
+
+    driver_opts:
+
+      parent: enp0s3
+
+    ipam:
+
+      driver: default
+
+      config:
+
+        - subnet: 192.168.1.0/24
+
+          gateway: 192.168.1.66' > docker-compose.yml
 
 
 sudo docker-compose up -d
 
 
-sudo ip route add 192.168.1.0/24 via 192.168.1.254 dev br0
+#sudo ip route add 192.168.1.0/24 via 192.168.1.254 dev br0
 
 
-sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o enp0s3 -j MASQUERADE
+#sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o enp0s3 -j MASQUERADE
