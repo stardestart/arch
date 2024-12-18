336acddbb1d40c76aa51c24e63d99901886024cc
stardestart <stardestart@icloud.com>
2024-12-18 14:26:41 +0300

    Rev_288

--- test.sh
+++ test.sh
@@ -1897,6 +1897,12 @@ for (( i=0; i<"${#massallaurprog[*]}"; i++ ))
                     then massaurprog+=( "$(yay -Ssq "${massallaurprog[$i]}" | grep --color=never ^"${massallaurprog[$i]}" | tail -n 1)" )
                 fi
             done
+            echo ${massaurprog[@]}
+            read -p "Нажмите ENTER для продолжения"
+            echo ${massprog[@]}
+            read -p "Нажмите ENTER для продолжения"
+            echo ${massallprog[@]}
+            read -p "Нажмите ENTER для продолжения"
 arch-chroot /mnt sudo -u "$username" yay -S "${massaurprog[@]}" --noconfirm --ask 4
 #
 #Автозапуск служб.
